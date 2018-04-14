f41ad24b9a006116656c12ed433ceda1ebeae76d
SamPratik <pratik.anwar@gmail.com>
2018-04-14 23:50:45 +0600

    Vue files and the root components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
   </div>
 </template>
 
@@ -10,19 +10,19 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'You first Vue App'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'Heeeeyyyy Cowboyyy!';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
