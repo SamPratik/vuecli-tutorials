2d65aa58d5c633bcddc32c23bb7359c62fb3fe4e
SamPratik <pratik.anwar@gmail.com>
2018-04-20 00:23:56 +0600

    v-model and v-model.lazy

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
   <div>
-      <keep-alive>
-          <component v-bind:is="component"></component>
-      </keep-alive>
-      <button type="button" v-on:click="component = 'form-one'">Form One</button>
-      <button type="button" v-on:click="component = 'form-two'">Form Two</button>
+      <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import FormOne from './components/form-one'
-import FormTwo from './components/form-two'
+
+import AddBlog from './components/addBlog.vue'
 
 export default {
   name: 'app',
   components: {
-    'form-one': FormOne,
-    'form-two': FormTwo
+      'add-blog': AddBlog
   },
   data() {
     return {
-      component: 'form-one'
+
     }
   }
 }
