430680bf432c693716172870e249258046bdcc43
SamPratik <pratik.anwar@gmail.com>
2018-04-22 00:30:39 +0600

    HTTP GET request with vue-resource

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-      <add-blog></add-blog>
+      <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
 
 import AddBlog from './components/addBlog.vue'
+import ShowBlogs from './components/showBlogs.vue'
 
 export default {
   name: 'app',
   components: {
-      'add-blog': AddBlog
+      'add-blog': AddBlog,
+      'show-blogs': ShowBlogs
   },
   data() {
     return {
