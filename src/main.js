f744f5797e7001e099b536d546cfb3344a18c7be
SamPratik <pratik.anwar@gmail.com>
2018-04-23 22:54:57 +0600

    locally registered custom directives and filters

--- src/main.js
+++ src/main.js
@@ -5,13 +5,6 @@ import VueResource from 'vue-resource'
 Vue.config.productionTip = false
 Vue.use(VueResource);
 
-// Custom Filters...
-// 'value' is the data on which this filter is applied to...
-// 'filter' doesn't change the real data it just chages the output on the DOM...
-Vue.filter('to-uppercase', function(value) {
-    return value.toUpperCase();
-});
-
 Vue.filter('snippet', function(value) {
     return value.slice(0,100) + '...';
 });
